118553864edcae858712ade142dd0a2550c3e857
Jianqiao <44894532+17tet@users.noreply.github.com>
2022-01-14 15:48:50 +0100

    7

--- players/player_7.py
+++ players/player_7.py
@@ -13,52 +13,26 @@ class Player:
     def play(self, turn_index: int, history: list[Turn], opponent) -> Action:
         
         if opponent.name == "Bojji":
-            have_cheat = False
-            if(turn_index == 0):
-                return Action.COOPERATE
-            for i in history:
-                    if(i["opponent_action"] == Action.CHEAT):                   
-                        have_cheat = True
-            if(have_cheat):
-                return Action.CHEAT
-            else:
-                return Action.COOPERATE
+            return Action.COOPERATE
         elif opponent.name == "Colsplif":
-            have_cheat = False
-            if(turn_index == 0):
-                return Action.COOPERATE
-            for i in history:
-                    if(i["opponent_action"] == Action.CHEAT):                   
-                        have_cheat = True
-            if(have_cheat):
-                return Action.CHEAT
-            else:
-                return Action.COOPERATE
+            return Action.CHEAT
 
         elif opponent.name == "Maxwelle":
-            if(turn_index == 0):
-                return Action.COOPERATE
-            else:
-                return Action.CHEAT
+            return Action.COOPERATE
         
         elif opponent.name == "Lucalixte":
-            if(turn_index == 0):
-                return Action.COOPERATE
-            else:
-                return Action.CHEAT
+            return Action.CHEAT
+            
         
         elif opponent.name == "Topone":
             
-            return Action.CHEAT
+            return Action.COOPERATE
         
         elif  opponent.name == "Cooplease":
             return Action.COOPERATE
 
 
         else:
-            if random.randint(0,2)>1:
-                return Action.COOPERATE
-            else:
-                return Action.CHEAT
+            return Action.COOPERATE
 
             
