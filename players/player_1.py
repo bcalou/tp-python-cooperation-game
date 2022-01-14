f07d2b0690edaaffa5d19bbe9205fe9891801091
Likarios <62408195+Likarios@users.noreply.github.com>
2022-01-14 13:40:31 +0100

    Player 1

--- players/player_1.py
+++ players/player_1.py
@@ -8,4 +8,22 @@ class Player:
     name: str = "Player 1"
 
     def play(turn_index: int, history: list[Turn]) -> Action:
-        return Action.COOPERATE
+        main_action = Action.COOPERATE
+        if turn_index == 1:
+            return Action.COOPERATE
+        elif turn_index == 2:
+            return Action.CHEAT
+        elif turn_index == 3:
+            return Action.COOPERATE
+        else:
+            three_first_turns = [history[0]["opponent_action"], history[1]["opponent_action"], history[2]["opponent_action"]]
+            if Action.CHEAT not in three_first_turns:
+                main_action = Action.CHEAT
+            elif Action.COOPERATE not in three_first_turns:
+                main_action = Action.COOPERATE
+            elif (history[0]["opponent_action"] == Action.COOPERATE and history[1]["opponent_action"] == Action.CHEAT and history[2]["opponent_action"] == Action.COOPERATE):
+                if turn_index % 2 == 0:
+                    main_action = main_action = Action.CHEAT
+                else:
+                    main_action = main_action = Action.COOPERATE
+            return main_action
