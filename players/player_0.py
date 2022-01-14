1b7ed3157d83a20d450d213b31c8be68f3247201
Anouk <anoukvangrieken@gmail.com>
2022-01-14 15:29:03 +0100

    ttt

--- players/player_0.py
+++ players/player_0.py
@@ -12,16 +12,20 @@ class Player:
         pass
 
     def play(self, turn_index: int, history: list[Turn], opponent) -> Action:
-        #punch=
-        if (opponent.name=="Rats" or opponent.name=="Bojji") :
-            print("Bande de rats. Les rats ne vaincront pas.")
-            return Action.CHEAT
-        elif (turn_index==0) :
-            print("Top 0 même au dessus du top 1")
-            return Action.CHEAT
+        punch = ["On parle de son sourire a la faire rougir",
+        "De ses yeux qui rendent amoureux",
+        "Trop petit trop de bide",
+        "Une meuf là bas m’fais les yeux doux",
+        "Grande, belles formes et regard revolver ",
+        "En tout cas garde la peche",
+        "J’suis un poisson, j cherche une poissonne",
+        "Que des belles croupe qui se balance",
+        "La j’ai vraiment trop chaud",
+        "Bois encore un verre, viens danser contre moi",
+        "En tout cas j'espère bien qu’on finira tous les deux !"]
+        act=[Action.CHEAT, Action.COOPERATE]
+        if (opponent.name =="Rats" and turn_index==0) :
+            print('\033[91m'+"Joyeux anniversaire Nico, tu es mon Top 1 à moi <3"+'\033[0m')
         else :
-            if (history[turn_index-1]["opponent_action"]== Action.CHEAT) :
-                return Action.CHEAT
-            else :
-                print("Que de l'amour pour mon Top 1")
-                return Action.COOPERATE
+            print('\033[92m'+random.choice(punch)+'\033[0m')
+        return random.choice(act)
