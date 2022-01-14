9976d1b8bf087f26abacd5027ed1e6caf1c90317
Landry Valetoux <58473063+landry42@users.noreply.github.com>
2022-01-14 13:39:25 +0100

    Ajout de print marrants

--- players/player_3.py
+++ players/player_3.py
@@ -5,11 +5,17 @@ class Player:
     turn_gain: int = 0
     cooperate_count: int = 0
     cheat_count: int = 0
-    name: str = "Landry et William"
+    name: str = "Landry et William [Coop cool]"
 
     def play(turn_index: int, history: list[Turn]) -> Action:
         if len(history) == 0:
+            print("Hey! Devenons amis!")
             return Action.COOPERATE
         else:
+            last_action : Action = history[-1]["opponent_action"]
+            if last_action == Action.COOPERATE:
+                print("C'est cool ça, continuons! :)")
+            else:
+                print("Héééééé! Pas cool :(")
             return history[-1]["opponent_action"]
         
