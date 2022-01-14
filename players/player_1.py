cf1183e97c14bf49e0cff65373472a7b3f27d46f
Likarios <62408195+Likarios@users.noreply.github.com>
2022-01-14 14:01:09 +0100

    Update player_1.py

--- players/player_1.py
+++ players/player_1.py
@@ -5,15 +5,15 @@ class Player:
     turn_gain: int = 0
     cooperate_count: int = 0
     cheat_count: int = 0
-    name: str = "Player 1"
+    name: str = "Rats"
 
     def play(turn_index: int, history: list[Turn]) -> Action:
         main_action = Action.COOPERATE
-        if turn_index == 1:
+        if turn_index == 0:
             return Action.COOPERATE
-        elif turn_index == 2:
+        elif turn_index == 1:
             return Action.CHEAT
-        elif turn_index == 3:
+        elif turn_index == 2:
             return Action.COOPERATE
         else:
             three_first_turns = [history[0]["opponent_action"], history[1]["opponent_action"], history[2]["opponent_action"]]
@@ -22,8 +22,9 @@ class Player:
             elif Action.COOPERATE not in three_first_turns:
                 main_action = Action.COOPERATE
             elif (history[0]["opponent_action"] == Action.COOPERATE and history[1]["opponent_action"] == Action.CHEAT and history[2]["opponent_action"] == Action.COOPERATE):
-                if turn_index % 2 == 0:
-                    main_action = main_action = Action.CHEAT
-                else:
-                    main_action = main_action = Action.COOPERATE
+                main_action = main_action = Action.CHEAT
+            elif (history[0]["opponent_action"] == Action.CHEAT and history[1]["opponent_action"] == Action.COOPERATE and history[2]["opponent_action"] == Action.CHEAT):
+                main_action = main_action = Action.CHEAT
+            else:
+                main_action = Action.CHEAT
             return main_action
