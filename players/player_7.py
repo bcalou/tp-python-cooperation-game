b60c76aa0ded8c6eadf72a0fc9d0890ba80181bc
Jianqiao <44894532+17tet@users.noreply.github.com>
2022-01-14 14:49:33 +0100

    7

--- players/player_7.py
+++ players/player_7.py
@@ -11,8 +11,37 @@ class Player:
         pass
 
     def play(self, turn_index: int, history: list[Turn], opponent) -> Action:
-        liste = [Action.COOPERATE, Action.CHEAT]
-        if turn_index == 1:
+        if opponent.name == "Bojji":
+            have_cheat : bool = False
+            if(turn_index >= 1):
+                for i in history:
+                    if(i["opponent_action"] == Action.CHEAT):                   
+                        have_cheat = True
+            if(turn_index == 9):
+                return Action.CHEAT
+            if(have_cheat):
+                return Action.CHEAT
+            else:
+                return Action.COOPERATE
+        elif opponent.name == "Colsplif":
+            if(turn_index == 0):
+                return Action.COOPERATE
+            for i in history:
+                    if(i["opponent_action"] == Action.CHEAT):                   
+                        have_cheat = True
+            if(have_cheat):
+                return Action.CHEAT
+            else:
+                return Action.COOPERATE
+        
+        elif opponent.name == "Topone":
+            
             return Action.CHEAT
+
         else:
-            return random.choice(liste, p=[0.66,0.33])
+            liste = [Action.COOPERATE, Action.CHEAT]
+            if turn_index == 1:
+                return Action.CHEAT
+            else:
+                return random.choice(liste)
+
