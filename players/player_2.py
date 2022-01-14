10a273a24e124dddefaad6b3a209b445069eeae6
LoickRbj <loick.roberjot@gmail.com>
2022-01-14 15:28:26 +0100

    Update player_2.py

--- players/player_2.py
+++ players/player_2.py
@@ -18,20 +18,25 @@ class Player:
         coop_count = 0
         if turn_index == 0:
             self.doom = False
+            if opponent.name == "Topone":
+                print("pas si tôt !")
+                return Action.CHEAT
         if turn_index >= 2:
             if opponent.name == "Rats":
                 print("Dératisation en cours")
                 self.doom = True
+            if opponent.name == "Topone" and turn_index == 2 :
+                coop_count += 1
             if self.doom:
                 return Action.CHEAT
             if history[turn_index-2]["opponent_action"] == Action.COOPERATE:
                 coop_count += 1
             if history[turn_index-1]["opponent_action"] == Action.COOPERATE:
                 coop_count += 1
-            if coop_count == 2:
+            if coop_count >= 2:
+                if opponent.name == "Bojji":
+                    return Action.COOPERATE
                 return Action.CHEAT
-            if coop_count == 1:
-                return Action.COOPERATE
             else:
                 print("Vous n'auriez pas dû...")
                 self.doom = True
