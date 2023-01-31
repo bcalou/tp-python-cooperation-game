b18fb0dc34cc4bb7881dac7fa53485810cca352e
bcalou <bastien.calou@gmail.com>
2023-01-31 23:27:27 +0100

    remove test code

--- cooperation/players/iota.py
+++ cooperation/players/iota.py
@@ -4,8 +4,3 @@ from cooperation.player import Player
 
 class Iota(Player):
     NAME = "Lucas"
-
-    def play(self, opponent: 'Player') -> Action:
-        """Choose what to do, cheat or cooperate"""
-        print(self._fight_history)
-        return Action.COOPERATE
