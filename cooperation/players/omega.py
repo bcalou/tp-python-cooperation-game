5020007a7de71e78584eb21e3f5ffe008d52da24
Antoine <antoine.martin885@gmail.com>
2023-02-02 16:45:02 +0100

    add omega first version

--- cooperation/players/omega.py
+++ cooperation/players/omega.py
@@ -1,5 +1,16 @@
 from cooperation.player import Player
+from cooperation.types import Action
 
 
 class Omega(Player):
     NAME = "Antoine"
+
+    Action = Action.COOPERATE
+
+    def play(self, opponent: 'Player') -> Action:
+        return Action.COOPERATE
+
+    def average_previous(self):
+        for previous_opponent in self._fight_history:
+            Action = previous_opponent["opponent_action"]
+
