2e801fd5c6e4192fa63a999e924225fd087b90e9
Roucool_PvP <lucastag77@gmail.com>
2023-02-02 16:44:06 +0100

    mon algo ultra poussé

--- cooperation/players/iota.py
+++ cooperation/players/iota.py
@@ -4,3 +4,7 @@ from cooperation.player import Player
 
 class Iota(Player):
     NAME = "Lucas"
+
+    def play(self, opponent: str) -> Action:
+        """Choose what to do, cheat or cooperate"""
+        return Action.CHEAT
