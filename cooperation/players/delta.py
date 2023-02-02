857d6ce95d440ac4cbcaab30485d69a9f51ce9a4
WillN-Git <wfndefo@gmail.com>
2023-02-02 16:45:18 +0100

    Will

--- cooperation/players/delta.py
+++ cooperation/players/delta.py
@@ -1,5 +1,12 @@
+from cooperation.types import Action
 from cooperation.player import Player
 
 
 class Delta(Player):
     NAME = "Wilfried"
+    C = 0
+
+    def play(self, opponent: str) -> Action:
+        """Choose what to do, cheat or cooperate"""
+        Delta.C += 1
+        return Action.CHEAT if Delta.C % 2 == 0 else Action.COOPERATE
