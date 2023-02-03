c4d4dc6233f807608e0397b18d7beedc08009ba2
JameFLC <jame.flochlecarour@gmail.com>
2023-02-03 11:47:12 +0100

    BQBQ

--- cooperation/players/beta.py
+++ cooperation/players/beta.py
@@ -1,5 +1,6 @@
 from cooperation.player import Player
 from cooperation.types import Action
+import pygame
 
 
 class Beta(Player):
@@ -8,13 +9,17 @@ class Beta(Player):
     def play(self, opponent: str) -> Action:
         """Choose what to do, cheat or cooperate"""
         cheat_ratio = 0
-        if opponent == "Lucas":
+        if opponent == "Pierre":
             return Action.CHEAT
 
         for game in self._fight_history:
             last_oponent_action = game["opponent_action"]
             if last_oponent_action == Action.CHEAT:
                 cheat_ratio += 1
+
+        pygame.mixer.init()
+        sound = pygame.mixer.Sound("./tkt/555418.mp3")
+        sound.play()  # Play the sound.
 
         fight_number = len(self._fight_history) + 1
 
