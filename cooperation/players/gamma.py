534a96ac87c61790bbfa17c58c898e9492932db5
JameFLC <jame.flochlecarour@gmail.com>
2023-02-03 11:47:26 +0100

    qweqeqe

--- cooperation/players/gamma.py
+++ cooperation/players/gamma.py
@@ -5,19 +5,19 @@ from cooperation.types import Action
 class Gamma(Player):
     NAME = "Ewen"
     friendly = {
-        "Colin" : True,
-        "Jame" : False,
-        "Wilfried" : True,
-        "Lucas" : False,
-        "Pierre-Baptiste" : True,
-        "Antoine" : True,
-        "Pierre" : True
+        "Colin": True,
+        "Jame": False,
+        "Wilfried": True,
+        "Lucas": False,
+        "Pierre-Baptiste": True,
+        "Antoine": True,
+        "Pierre": True
     }
 
-    def play(self, opponent: Player, ) -> Action:
-        print(opponent.NAME)
-        return Action.CHEAT
-        
+    def play(self, opponent) -> Action:
+
+        return Action.COOPERATE
+
         """if len(self._fight_history)%10 == 0: 
             if self.friendly[opponent.NAME]:
                 return Action.COOPERATE
