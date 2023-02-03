5fcd056dd934be2bf9b2bc3c0dec96fe5a0a90bb
WillN-Git <wfndefo@gmail.com>
2023-02-03 11:13:16 +0100

    azerty

--- cooperation/players/delta.py
+++ cooperation/players/delta.py
@@ -5,9 +5,11 @@ import random
 
 class Delta(Player):
     NAME = "Wilfried"
+    prev_score = 0
 
     def play(self, opponent: str) -> Action:
         """Choose what to do, cheat or cooperate"""
-        arr = [Action.CHEAT, Action.COOPERATE]
 
-        return random.choice(arr)
+        action = Action.CHEAT if self.score <= Delta.prev_score else Action.COOPERATE
+        Delta.prev_score = self.score
+        return action
