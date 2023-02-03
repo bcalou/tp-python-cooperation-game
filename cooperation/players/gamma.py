7489b4942fdfa4ab7538e1e4fcfe6dbcd97524ba
EwenJezCNAM <r1bo.gamedesign@gmail.com>
2023-02-03 09:25:40 +0100

    pushEwen03_02_23

--- cooperation/players/gamma.py
+++ cooperation/players/gamma.py
@@ -1,7 +1,30 @@
 from cooperation.player import Player
 from cooperation.types import Action
 
+
 class Gamma(Player):
     NAME = "Ewen"
+    friendly = {
+        "Colin" : True,
+        "Jame" : False,
+        "Wilfried" : True,
+        "Lucas" : False,
+        "Pierre-Baptiste" : True,
+        "Antoine" : True,
+        "Pierre" : True
+    }
+
     def play(self, opponent: Player, ) -> Action:
-        return Action.COOPERATE
+        if len(self._fight_history)%10 == 0: 
+            if self.friendly[opponent.NAME]:
+                return Action.COOPERATE
+            else :
+                return Action.CHEAT
+
+        elif self._fight_history[-1].opponent_action == Action.CHEAT:
+            self.friendly[opponent.NAME] = False
+
+        if self.friendly[opponent.NAME]:
+            return Action.COOPERATE
+        else :
+            return Action.CHEAT
