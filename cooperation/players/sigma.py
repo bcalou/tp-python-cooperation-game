55f929e43d466591bc9dcaf55d4c61442245d52c
PierreVandel <pierre.vdl@orange.fr>
2023-02-03 09:37:52 +0100

    second fight

--- cooperation/players/sigma.py
+++ cooperation/players/sigma.py
@@ -1,8 +1,23 @@
 from cooperation.player import Player
 from cooperation.types import Action, Turn
+import random
 
 
 class Sigma(Player):
     NAME = "Pierre"
 
-    
+    def play(self, opponent: str) -> Action:
+
+        self.prepare_new_fight()
+
+        if opponent == 'Lucas' or opponent == 'Jame':
+            self._say('Pour toujours avoir triché lors de la dernière partie')
+            return Action.CHEAT
+            
+        else:
+            #if self.get_cooperation_percenatage() >=
+            return Action.COOPERATE if random.randbytes(1) == 1 else Action.CHEAT
+                
+            
+        
+
