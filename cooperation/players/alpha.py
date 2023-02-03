c03c662e824b0a68987ab27f97870533af531ccf
Colin BERNARD <colin.bernard@ymail.com>
2023-02-03 10:05:45 +0100

    yolo

--- cooperation/players/alpha.py
+++ cooperation/players/alpha.py
@@ -8,9 +8,6 @@ class Alpha(Player):
     NAME = "Colin"
     _turn: int = 0
 
-    def mapValue(self, value: float, inMin: float, inMax: float, outMin: float, outMax: float):
-        return outMin + (float(value - inMin) / float(inMax - inMin) * (outMax
-                        - outMin))
 
     def play(self, opponent: str) -> Action:
         self._turn += 1
@@ -18,11 +15,15 @@ class Alpha(Player):
         if self._turn == 1:
             return Action.COOPERATE
         else:
-            return Action.COOPERATE if self._fight_history[-1]['opponent_action'] == Action.COOPERATE else Action.CHEAT
+            return Action.COOPERATE if self._fight_history[-1]['opponent_action'] == Action.CHEAT else Action.COOPERATE
 
 
 
         #####################    THINKING DELAY    ############################
+        # def mapValue(self, value: float, inMin: float, inMax: float, outMin: float, outMax: float):
+        #     return outMin + (float(value - inMin) / float(inMax - inMin) * (outMax
+        #                     - outMin))
+
         # reflexionTime: float = self.mapValue(random.random(), 0, 1, 1, 5)
         # beginTime: float = time.time()
         # currentTime: float = time.time()
