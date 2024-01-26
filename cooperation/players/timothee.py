f77402583de68aed0e0ef763eec287196b6cc05d
Timo <timetais22@gmail.com>
2024-01-26 14:50:07 +0100

    timo r1

--- cooperation/players/timothee.py
+++ cooperation/players/timothee.py
@@ -6,4 +6,26 @@ class Timothee(Player):
     NAME = "Timothee"
 
     def play(self, opponent: str) -> Action:
+        match opponent:
+            case "Badr":
+                return Action.COOPERATE
+            case "Baptiste":
+                return Action.CHEAT
+            case "Ewen":
+                return Action.COOPERATE
+            case "Hugo":
+                return Action.CHEAT
+            case "Joffrey":
+                return Action.CHEAT
+            case "Kayyissa":
+                return Action.CHEAT
+            case "Lois":
+                return Action.COOPERATE
+            case "Marius":
+                return Action.CHEAT
+            case "Nathan":
+                return Action.COOPERATE
+            case "Theo":
+                return Action.COOPERATE
         return Action.COOPERATE
+
