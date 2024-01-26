c4d95617aba3400069c0abab75f8b4360042661a
Hugo <53018953+YannoteX@users.noreply.github.com>
2024-01-26 15:35:49 +0100

    add hugo

--- cooperation/players/hugo.py
+++ cooperation/players/hugo.py
@@ -1,5 +1,6 @@
 from cooperation.player import Player
 from cooperation.types import Action
+import random
 
 
 class Hugo(Player):
@@ -10,15 +11,28 @@ class Hugo(Player):
         mother_fuckers: list[str]
         gud_guys: list[str]
 
-        if opponent == "Kayyissa":
-            self._say("Sorry mam, life's hard wiv wemen")
+        cool_score: float = self.get_cool_score()
+
+        if cool_score >= 0.65:
+
+            return (random.choices([Action.CHEAT, Action.COOPERATE], [0.1, 0.9])[0])
 
         else:
-            self._say("F*CK U AHAHAH !!!!!!")
 
-        return Action.CHEAT
+            self._say("U smart a**, waddya think u doin ?")
 
+            return Action.CHEAT
 
-def is_gud(str):
+    def get_ennemy_cool_score(self) -> float:
 
-    return 0
+        cool_factor = 0
+
+        for i in self._fight_history:
+            if i.opponent_action == Action.COOPERATE:
+
+                cool_factor += 1
+
+        return cool_factor/len(self._fight_history)
+
+    def get_my_cool_score(self, ennemy_cool_score):
+        return 0
