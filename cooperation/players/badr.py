0eb119be7bb303ad2dcd3891930f851124516a0a
BadMisterH <60579407+BadMisterH@users.noreply.github.com>
2024-01-26 15:58:00 +0100

    test4

--- cooperation/players/badr.py
+++ cooperation/players/badr.py
@@ -1,28 +1,34 @@
-import random
 from cooperation.player import Player
 from cooperation.types import Action
+import random
 
 class Badr(Player):
     NAME = "Badr"
     history = {}
+    opponent_history = {}
 
     INITIAL_COOPERATE_WEIGHT = 0.4
     INITIAL_CHEAT_WEIGHT = 0.6
+    FORGIVENESS_CHANCE = 0.1 
 
     def play(self, opponent: str) -> Action:
-
         if opponent not in self.history:
             return random.choices(
                 [Action.COOPERATE, Action.CHEAT], 
                 weights=[self.INITIAL_COOPERATE_WEIGHT, self.INITIAL_CHEAT_WEIGHT], 
                 k=1
             )[0]
-        
-        return self.history[opponent]
+
+        last_opponent_action = self.opponent_history[opponent][-1]
+        if last_opponent_action == Action.CHEAT and random.random() < self.FORGIVENESS_CHANCE:
+            return Action.COOPERATE
+        return last_opponent_action
 
     def result(self, opponent: str, my_action: Action, opponent_action: Action, result: int):
         if isinstance(opponent_action, Action):
-            self.history[opponent] = opponent_action
+            if opponent not in self.opponent_history:
+                self.opponent_history[opponent] = []
+            self.opponent_history[opponent].append(opponent_action)
         else:
             raise ValueError("L'action de l'adversaire doit être une instance de Action.")
 
