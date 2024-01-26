a1c7a8787272cdf573036f9eca417b98ce376449
Kayyissa <kayyissa@haissous.name>
2024-01-26 15:04:21 +0100

    kayou

--- cooperation/players/kayyissa.py
+++ cooperation/players/kayyissa.py
@@ -6,10 +6,11 @@ class Kayyissa(Player):
     NAME = "Kayyissa"
 
     def play(self, opponent: str) -> Action:
-        cheat: bool = False
+        
+        cheat: bool = True
         for turn in self._fight_history:
-            if turn["opponent_action"] == Action.CHEAT:
-                cheat = True
+            if turn["opponent_action"] == Action.COOPERATE:
+                cheat = False
         if cheat:
             return Action.CHEAT
         return Action.COOPERATE
