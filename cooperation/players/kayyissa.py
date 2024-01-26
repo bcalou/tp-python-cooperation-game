f486df14afea4e52f228c9b3de2473e7f1cc4a3a
Kayyissa <kayyissa@haissous.name>
2024-01-26 14:51:08 +0100

    Update kayyissa.py

--- cooperation/players/kayyissa.py
+++ cooperation/players/kayyissa.py
@@ -6,4 +6,10 @@ class Kayyissa(Player):
     NAME = "Kayyissa"
 
     def play(self, opponent: str) -> Action:
+        cheat: bool = False
+        for turn in self._fight_history:
+            if turn["opponent_action"] == Action.CHEAT:
+                cheat = True
+        if cheat:
+            return Action.CHEAT
         return Action.COOPERATE
