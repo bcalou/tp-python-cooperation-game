ca664043e7f98d30a2c37e57ab802939382c3dec
bterral <baptiste.terral.auditeur@lecnam.net>
2024-01-26 14:50:15 +0100

    baptiste

--- cooperation/players/baptiste.py
+++ cooperation/players/baptiste.py
@@ -6,4 +6,12 @@ class Baptiste(Player):
     NAME = "Baptiste"
 
     def play(self, opponent: str) -> Action:
-        return Action.COOPERATE
+        
+        if opponent == "Nathan":
+            return Action.CHEAT
+
+        if not self._game_history:
+            return Action.COOPERATE
+        else:
+            last_turn = self._game_history[-1]
+            return last_turn["opponent_action"]
