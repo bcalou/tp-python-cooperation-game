f2f38ffc4045c9e9d599e7387649397b556f997c
Nathan Deroche <sillybob03@gmail.com>
2024-01-26 15:11:47 +0100

    nathan round 2

--- cooperation/players/nathan.py
+++ cooperation/players/nathan.py
@@ -6,28 +6,26 @@ class Nathan(Player):
     NAME = "Nathan"
 
     def play(self, opponent: str) -> Action:
-        match opponent:
-            case "Hugo":
-                return Action.CHEAT
-            case "Badr":
-                return Action.COOPERATE
-            case "Baptiste":
-                return Action.CHEAT
-            case "Ewen":
-                return Action.COOPERATE
-            case "Joffrey":
-                return Action.COOPERATE
-            case "Kayyissa":
-                return Action.CHEAT
-            case "Lois":
-                return Action.COOPERATE
-            case "Marius":
-                return Action.COOPERATE
-            case "Théo":
-                return Action.COOPERATE
-            case "Timothee":
-                return Action.CHEAT
-            case _:
-                return Action.COOPERATE
+
+        if opponent == "Ewen":
+            return Action.CHEAT
+
+        times_cheated = 0
+        times_cooperated = 0
+
+        if len(self._fight_history) == 0:
+            return Action.CHEAT
+
+        for opponent_action in self._fight_history:
+            if opponent_action == Action.CHEAT:
+                times_cheated += 1
+            else:
+                times_cooperated += 1
+
+        probability_of_cheating = (times_cheated * 100) / len(self._fight_history)
+
+        if probability_of_cheating > 50:
+            return Action.CHEAT
 
         return Action.COOPERATE
+
