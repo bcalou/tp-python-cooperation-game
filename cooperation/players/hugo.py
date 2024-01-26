85898c06ecdcb786090a795c61135a45bd3958cd
Hugo <53018953+YannoteX@users.noreply.github.com>
2024-01-26 16:09:42 +0100

    add hugo

--- cooperation/players/hugo.py
+++ cooperation/players/hugo.py
@@ -13,9 +13,14 @@ class Hugo(Player):
 
         his_cool_score: float = self.get_ennemy_cool_score()
 
-        if his_cool_score >= 0.65:
+        if opponent == "Ewen":
 
-            return (random.choices([Action.CHEAT, Action.COOPERATE], [0.1, 0.9])[0])
+            self._say("Azy joue avec moi")
+            return Action.CHEAT
+
+        elif his_cool_score >= 0.6:
+
+            return (random.choices([Action.CHEAT, Action.COOPERATE], [0.05, 0.95])[0])
 
         else:
 
@@ -27,12 +32,29 @@ class Hugo(Player):
 
         cool_factor = 0
 
+        if len(self._fight_history) == 0:
+            return 1
+
         for i in self._fight_history:
-            if i.opponent_action == Action.COOPERATE:
+            if i["opponent_action"] == Action.COOPERATE:
 
                 cool_factor += 1
 
         return cool_factor/len(self._fight_history)
 
     def get_my_cool_score(self, ennemy_cool_score):
+
+        cool_factor = 0
+
+        if len(self._fight_history) == 0:
+            return 1
+
+        for i in self._fight_history:
+
+            if i["opponent_action"] == Action.CHEAT:
+                cool_factor += 1
+
+            elif i["opponent_action"] == Action.COOPERATE and i["self_action"] == Action.COOPERATE:
+                cool_factor += 1
+
         return 0
