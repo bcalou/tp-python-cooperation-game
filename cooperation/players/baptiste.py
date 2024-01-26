fef1e2d5b123f6a0d3cab10619aad338f3ba8523
bterral <baptiste.terral.auditeur@lecnam.net>
2024-01-26 15:34:04 +0100

    non en fait

--- cooperation/players/baptiste.py
+++ cooperation/players/baptiste.py
@@ -11,7 +11,7 @@ class Baptiste(Player):
         else:
             recent_fights = self._fight_history[-10:]
             cheat_count = sum(turn["opponent_action"] == Action.CHEAT for turn in recent_fights)
-            if cheat_count < 5:
+            if cheat_count > 5:
                 return Action.CHEAT
             else:
                 return Action.COOPERATE
