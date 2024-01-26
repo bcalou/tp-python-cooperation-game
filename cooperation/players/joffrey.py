53e6ebdacd20767e444871fe7b1768660ea3bed4
joffrey3 <fouche.joffrey@gmail.com>
2024-01-26 15:04:36 +0100

    joffrey push

--- cooperation/players/joffrey.py
+++ cooperation/players/joffrey.py
@@ -7,10 +7,4 @@ class Joffrey(Player):
     NAME = "Joffrey"
 
     def play(self, opponent: str) -> Action:
-        return Action.COOPERATE
-    '''
-    if(len(self._fight_history)==0){
-        return Action.COOPERATE
-    }
-    return self._fight_history[-1]["opponent_action"];
-    '''
+        return Action.COOPERATE if len(self._fight_history)==0 else self._fight_history[-1]["opponent_action"]
