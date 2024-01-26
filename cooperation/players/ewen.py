eb878755bf3a245235e6d8ab51ec2176fb3219cc
The17thDoctor <40616434+The17thDoctor@users.noreply.github.com>
2024-01-26 14:49:32 +0100

    Céfai

--- cooperation/players/ewen.py
+++ cooperation/players/ewen.py
@@ -1,3 +1,4 @@
+from cooperation.log import Log
 from cooperation.player import Player
 from cooperation.types import Action
 
@@ -5,5 +6,11 @@ from cooperation.types import Action
 class Ewen(Player):
     NAME = "Ewen"
 
+    __last_action: Action
+
+    def __init__(self, log: Log):
+        super().__init__(log)
+        self.__last_action = Action.CHEAT
+
     def play(self, opponent: str) -> Action:
-        return Action.COOPERATE
+        return Action.CHEAT
