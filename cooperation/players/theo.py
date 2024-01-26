86aa5b6e6d3913dd48f96cf88c73a37386dbd4f5
TheoThenault <theo.thenault@gmail.com>
2024-01-26 15:10:46 +0100

    Théo v2

--- cooperation/players/theo.py
+++ cooperation/players/theo.py
@@ -1,11 +1,18 @@
 from cooperation.player import Player
-from cooperation.types import Action
+from cooperation.types import Action, Turn
 
 
 class Theo(Player):
     NAME = "Théo"
-    count: int = 0
 
     def play(self, opponent: str) -> Action:
-        self.count += 1
-        return Action.COOPERATE if self.count%2==0 else Action.CHEAT
+        if(len(self._game_history) <= 0):
+            return Action.COOPERATE
+
+        first_turn: Turn = self._game_history[0]
+        print(first_turn)
+
+        action: Action = first_turn.get("opponent_action")
+        print("Action", action)
+
+        return Action.COOPERATE if action == Action.COOPERATE else Action.CHEAT
