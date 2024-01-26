f154bff2cbe2ae098033609e77a50a7d0af556cd
bterral <baptiste.terral.auditeur@lecnam.net>
2024-01-26 15:08:41 +0100

    baptiste

--- cooperation/players/baptiste.py
+++ cooperation/players/baptiste.py
@@ -6,12 +6,12 @@ class Baptiste(Player):
     NAME = "Baptiste"
 
     def play(self, opponent: str) -> Action:
-        
-        if opponent == "Nathan":
-            return Action.CHEAT
-
-        if not self._game_history:
+        if len(self._fight_history) < 10:
             return Action.COOPERATE
         else:
-            last_turn = self._game_history[-1]
-            return last_turn["opponent_action"]
+            recent_fights = self._fight_history[-10:]
+            cheat_count = sum(turn["opponent_action"] == Action.CHEAT for turn in recent_fights)
+            if cheat_count > 5:
+                return Action.CHEAT
+            else:
+                return Action.COOPERATE
